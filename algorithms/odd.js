f1ffecee8fbef13f37218bfadea2396af5ea8723
emily renee <hagoodem@gmail.com>
2017-07-24 16:06:06 -0400

    add algorithms answers

---
+++ algorithms/odd.js
@@ -0,0 +1,7 @@
+function isOdd(numb) {
+	if (numb % 2 !== 0) {
+		console.log("The number " + numb + " is odd");
+	} else {
+		console.log("The number " + numb + " is NOT odd");	
+	}
+}
---
+++ algorithms/power-of-two.js
@@ -0,0 +1,7 @@
+function isPowerofTwo(numb) {
+	if (numb && (number & (numb -1)) === 0) {
+		console.log("The number " + numb + " is a power of 2");
+	} else {
+		console.log("The number " + numb + " is NOT a power of 2");	
+	}
+}

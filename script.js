ea9c8dbce2a4101d6d1d4ca4e619190b31fccd76
emily renee <hagoodem@gmail.com>
2017-07-24 14:33:29 -0400

    write data to file

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node 
 
 var axios = require("axios");
-axios.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=cat")
+var jsonfile = require("jsonfile");
+
+
+axios.get("https://api.flickr.com/services/feeds/photos_public.gne?format=json&tags=cat&nojsoncallback=?")
   .then(function (response) {
-    console.log(response);
+	// write response data to result.json
+	jsonfile.writeFile("result.json", response.data);
+	console.log("Cat the result.json file.");
   })
   .catch(function (error) {
     console.log(error);
